a3c08803f96ba28ad0dc6f306e8f7b2fa9dcf130
Sebastianpobletejacob <sipoblete@uc.cl>
2020-05-02 20:33:55 -0400

    ./app

--- src/models/index.js
+++ src/models/index.js
@@ -7,8 +7,8 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env]
 const db = {};
-// console.log("path");
-// console.log(__dirname);
+console.log("path");
+console.log(__dirname);
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
