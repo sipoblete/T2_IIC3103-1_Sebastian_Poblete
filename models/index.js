ff28dd10e3e094b947f2cded2210bef340789666
Sebastianpobletejacob <sipoblete@uc.cl>
2020-05-02 21:23:22 -0400

    move config backwards

--- models/index.js
+++ models/index.js
@@ -5,10 +5,9 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../../config/config.json')[env]
+const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
-console.log("path");
-console.log(__dirname);
+
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
