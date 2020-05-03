5913fa03753bb9d2c505c91452a2dd88929565d0
Sebastianpobletejacob <sipoblete@uc.cl>
2020-05-02 22:03:46 -0400

    cambio

--- src/models/index.js
+++ src/models/index.js
@@ -6,10 +6,14 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 
-// const config = require(__dirname + '/../config/config.json')[env]
+let config;
+if (env==="development") {
+  config = require(__dirname + '/../config/config.json')[env]
+}
 const db = {};
 
 let sequelize;
+
 if (env==="production") {
   sequelize = new Sequelize(process.env["DATABASE_URL"], {});
 } else {
