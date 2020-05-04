088abeaffdfabb556ce2ec7770a4099af2de5644
Sebastianpobletejacob <sipoblete@uc.cl>
2020-05-04 18:47:05 -0400

    index cambiado

--- src/models/index.js
+++ src/models/index.js
@@ -1,25 +1,32 @@
 'use strict';
-import { readdirSync } from 'fs';
-import { basename as _basename, join } from 'path';
-import Sequelize from 'sequelize';
-const basename = _basename(__filename);
+
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+
+let config;
+if (env==="development") {
+  config = require(__dirname + '/../config/config.json')[env]
+}
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
- sequelize = new Sequelize(process.env[config.use_env_variable], config);
+
+if (env==="production") {
+  sequelize = new Sequelize(process.env["DATABASE_URL"], {});
 } else {
- sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-readdirSync(__dirname)
+fs
+  .readdirSync(__dirname)
   .filter(file => {
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
-    const model = sequelize['import'](join(__dirname, file));
+    const model = sequelize['import'](path.join(__dirname, file));
     db[model.name] = model;
   });
 
@@ -32,4 +39,4 @@ Object.keys(db).forEach(modelName => {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-export default db;
+module.exports = db;
